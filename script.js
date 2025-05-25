bf3a6584a30f6340c9f49c049194ac835b8a517c
Vikas Kumar <vikas@git.training>
2025-05-25 22:26:09 +0530

    add html

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to Vikas' Portfolio!");
+    
+    // Example: Add an alert when clicking on certifications
+    document.querySelector("#certifications").addEventListener("click", function() {
+        alert("Explore my certifications!");
+    });
+});
